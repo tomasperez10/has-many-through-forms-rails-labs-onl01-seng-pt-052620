2a0f56d03860e053f1c89101b1b1645a2d8cd10e
Tomas Perez <tomasitopg6@gmail.com>
2020-10-14 17:04:44 +0000

    Automatically backed up by Learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,9 @@
 class CommentsController < ApplicationController
 
   def create
+    if params[:comment][:user_id].blank?
+      params[:comment].delete(:user_id)
+    end
     comment = Comment.create(comment_params)
     redirect_to comment.post
   end
