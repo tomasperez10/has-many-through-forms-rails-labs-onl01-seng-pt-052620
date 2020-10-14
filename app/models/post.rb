796acdbd52079f36522c360f2578b95390d1c548
Tomas Perez <tomasitopg6@gmail.com>
2020-10-14 17:03:49 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,16 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories
 
-  
+  def categories_attributes=(categories_attributes)
+   categories_attributes.values.each do |category_attributes|
+     if category_attributes[:name].present?
+       category = Category.find_or_create_by(category_attributes)
+       if !self.categories.include?(category)
+         self.post_categories.build(:category => category)
+       end
+     end
+   end
+ end
 
 
 end
